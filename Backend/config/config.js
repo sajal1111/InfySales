ddc8ebae7924d707bd0619b5c91245458d3d425d
sajal1111 <103333121+sajal1111@users.noreply.github.com>
2026-07-07 19:01:40 +0530

    complete

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,3 +1,2 @@
-// var url="mongodb+srv://sajal:7NJP7XkQjCMe7DG9@reactjs.k9sse.mongodb.net/?retryWrites=true&appName=reactjs";
-var url="mongodb+srv://sajalsowna9:77aI9xNqMkv6abSg@cluster0.jfoh7.mongodb.net/?appName=Cluster0";
+var url="mongodb+srv://sajalsowna9:password@cluster0.jfoh7.mongodb.net/?appName=Cluster0";
 module.exports={url};
